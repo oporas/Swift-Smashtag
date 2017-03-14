534d38270e9525e1dccc951c1e79674934425096
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-03-14 19:49:16 +0200

    Simple Twitter TableView

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by ooras on 14/03/2017.
+//  Copyright © 2017 oporas. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    @IBOutlet weak var tweetUserLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweet: Twitter.Tweet? {
+        didSet { updateUI() }
+    }
+
+    private func updateUI() {
+        tweetTextLabel?.text = tweet?.text
+        tweetUserLabel?.text = tweet?.user.description
+        
+        if let profileImageURL = tweet?.user.profileImageURL {
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                if let imageData = try? Data(contentsOf: profileImageURL) {
+                    DispatchQueue.main.async {
+                        self?.tweetProfileImageView?.image = UIImage(data: imageData)
+                    }
+                }
+            }
+
+        } else {
+            tweetProfileImageView?.image = nil
+        }
+        
+        if let created = tweet?.created {
+            let formatter = DateFormatter()
+            if Date().timeIntervalSince(created) > 24*60*60 {
+                formatter.dateStyle = .short
+            } else {
+                formatter.timeStyle = .short
+            }
+            tweetCreatedLabel?.text = formatter.string(from: created)
+        } else {
+            tweetCreatedLabel?.text = nil
+        }
+    }
+}
